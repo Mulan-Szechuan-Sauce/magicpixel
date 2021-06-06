aaa01384041413ac41af3cd244dcbe1556cbce00
Elijah Mirecki <elimirks@gmail.com>
2021-06-05 22:23:19 -0400

    Start migrating to SDL / raw GL

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,5 @@
-use sfml::graphics::{
-    RenderWindow, RenderTarget, Color, RectangleShape,
-    Transformable, Text, Font, Texture, Sprite
-};
-use sfml::window::{VideoMode, Event, Style, Key};
-use sfml::window::mouse::{Button};
-use sfml::system::{Vector2i, Vector2f, Clock};
-use std::convert::{TryInto};
-use sfml::SfBox;
-
-mod ui_grid;
-use ui_grid::draw_overlay;
+extern crate sdl2;
+extern crate gl;
 
 mod physics;
 use physics::Physics;
@@ -17,11 +7,14 @@ use physics::Physics;
 mod grid;
 use grid::*;
 
-mod fps;
-use fps::{FpsCounter};
-
 mod render;
 use render::*;
+
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+
+use std::time::{SystemTime};
 
 pub struct RenderContext {
     pub scale: f32,
@@ -31,10 +24,6 @@ pub struct RenderContext {
     pub grid_height: i32,
     pub mouse_x: i32,
     pub mouse_y: i32,
-    pub rect: RectangleShape<'static>,
-    pub font: SfBox<Font>,
-    pub display_texture: SfBox<Texture>,
-    pub display_pixels: Box<[u8]>,
 }
 
 impl RenderContext {
@@ -47,104 +36,33 @@ impl RenderContext {
     }
 }
 
-fn create_simple_grid() -> ParticleGrid {
-    #[allow(unused_mut)]
-    let mut grid = ParticleGrid::new(100, 100);
-    //let mut grid = ParticleGrid::new(2, 4);
+fn find_sdl_gl_driver() -> Option<u32> {
+    for (index, item) in sdl2::render::drivers().enumerate() {
+        if item.name == "opengl" {
+            return Some(index as u32);
+        }
+    }
+    None
+}
 
-    // for x in 0..100 {
-    //     grid.set(x, 49, Particle {
-    //         p_type: ParticleType::Water,
-    //         fill_ratio: 1,
-    //         ..Default::default()
-    //     });
-    // }
+fn create_simple_grid() -> ParticleGrid {
+    let mut grid = ParticleGrid::new(100, 100);
+
+    for x in 69..80 {
+        for y in 0..1 {
+            grid.set(x, y, Particle {
+                p_type: ParticleType::Water,
+                fill_ratio: 1,
+                ..Default::default()
+            });
+        }
+    }
 
     grid
 }
 
-fn render_grid(window: &mut RenderWindow, context: &mut RenderContext, grid: &ParticleGrid) {
-    for x in 0..grid.width {
-        for y in 0..grid.height {
-            let p = grid.get(x, y);
-            let scale = context.scale as usize;
-            let fill_amount = (255u64 * p.fill_ratio as u64 / MAX_FILL as u64) as u8;
-
-            for s in 0..(scale*scale) {
-                // This may be slow
-                let x_initial = x as usize * scale + s % scale;
-                let y_initial = y as usize * scale + s / scale;
-                let i = 4 * (x_initial + y_initial * scale * grid.width as usize);
-
-                unsafe {
-                    let pixel_ptr: *mut u8 =
-                        context.display_pixels.as_mut_ptr().add(i);
-
-                    match p.p_type {
-                        ParticleType::Water => {
-                            *pixel_ptr.offset(0) = 0;
-                            *pixel_ptr.offset(1) = 0;
-                            *pixel_ptr.offset(2) = 255;
-                            *pixel_ptr.offset(3) = fill_amount;
-                        },
-                        _ => {
-                            *pixel_ptr.offset(0) = 0;
-                            *pixel_ptr.offset(1) = 0;
-                            *pixel_ptr.offset(2) = 0;
-                            *pixel_ptr.offset(3) = 0;
-                        }
-                    };
-                }
-            }
-        }
-    }
-
-    unsafe {
-        context.display_texture.update_from_pixels(
-            &context.display_pixels,
-            grid.width as u32 * context.scale as u32,
-            grid.height as u32 * context.scale as u32,
-            0,
-            0 
-        );
-    }
-
-    let s = Sprite::with_texture(&context.display_texture);
-    window.draw(&s);
-}
-
-fn insert_particle(
-    grid: &mut ParticleGrid,
-    context: &RenderContext,
-    p_type: &ParticleType
-) {
-    let x = (context.mouse_x as f32 / context.scale) as i32;
-    let y = (context.mouse_y as f32 / context.scale) as i32;
-
-    if grid.in_bounds(x, y) {
-        grid.set(x, y, Particle {
-            p_type: p_type.clone(),
-            ..Default::default()
-        });
-    }
-}
-
-fn sum_water(grid: &ParticleGrid) -> u64 {
-    let mut net_fill_ratio: u64 = 0;
-
-    for particle in &grid.grid {
-        if particle.p_type == ParticleType::Water {
-            net_fill_ratio += particle.fill_ratio as u64
-        }
-    }
-
-    return net_fill_ratio;
-}
-
-fn main() {
+pub fn main() {
     let grid = create_simple_grid();
-
-    let desktop = VideoMode::desktop_mode();
 
     let max_win_width = 2400.0;
     let max_win_height = 1400.0;
@@ -157,9 +75,6 @@ fn main() {
     let win_width = (grid.width as f32 * scale).ceil() as u32;
     let win_height = (grid.height as f32 * scale).ceil() as u32;
 
-    let pixel_count = win_width as usize * win_height as usize;
-    let rgba_pixel_count = 4 * pixel_count;
-
     let mut context = RenderContext {
         scale: scale,
         win_width: win_width,
@@ -168,91 +83,66 @@ fn main() {
         grid_height: grid.height,
         mouse_x: 0,
         mouse_y: 0,
-        rect: RectangleShape::with_size(Vector2f::new(scale, scale)),
-        // FIXME:
-        font: Font::from_file("/home/elijah/code/magicpixel/assets/Jura-Medium.ttf").unwrap(),
-        display_texture: Texture::new(win_width, win_height).unwrap(),
-        display_pixels: vec![0; rgba_pixel_count].into_boxed_slice(),
     };
-
-    let mut window = RenderWindow::new(
-        VideoMode::new(win_width, win_height, desktop.bits_per_pixel),
-        "Ok zoomer",
-        Style::CLOSE,
-        &Default::default()
-    );
-
-    window.set_position(Vector2i::new(
-        ((desktop.width - win_width) / 2).try_into().unwrap(),
-        ((desktop.height - win_height) / 2).try_into().unwrap()
-    ));
-
-    let clock = Clock::start();
-    let mut prev_tick = 0;
-    let tick_time = 0.05;
-    let mut is_paused = true;
-    let mut is_depressed = false;
-    let mut draw_p_type = ParticleType::Water;
-
-    // FIXME: don't clone this, use Rc
-    let fps_font = context.font.clone();
-    let debug_font = context.font.clone();
-    let mut fps_counter = FpsCounter::new(&fps_font);
 
     let mut physics = Physics::new(grid);
 
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+ 
+    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+        .position_centered()
+        .opengl()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas()
+        .index(find_sdl_gl_driver().unwrap())
+        .build()
+        .unwrap();
+ 
+    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.clear();
+    canvas.present();
+
+    // initialization
+    gl::load_with(|name| video_subsystem.gl_get_proc_address(name) as *const _);
+
+    // sdl::render creates a context for you, if you use a Canvas you need to use it.
+    let _ = canvas.window().gl_set_context_to_current();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    let program_epoch = SystemTime::now();
+    let tick_time = 0.05;
+    let mut prev_tick = 0;
+    let is_paused = false;
+
     let mut renderer = GlslRenderer::new("assets/grid.frag".to_string(), &context);
 
-    while window.is_open() {
-        // Event processing
-        while let Some(event) = window.poll_event() {
-            // Request closing for the window
+    'running: loop {
+        for event in event_pump.poll_iter() {
             match event {
-                Event::Closed |
-                Event::KeyPressed { code: Key::ESCAPE, .. } =>
-                    window.close(),
-                Event::KeyPressed { code: Key::P, .. } =>
-                    is_paused = !is_paused,
-                Event::KeyPressed { code: Key::R, .. } =>
-                    physics = Physics::new(create_simple_grid()),
-                Event::KeyReleased { code: Key::SPACE, .. } =>
-                    physics.update(),
-                Event::MouseWheelScrolled { .. } => {
-                    // TODO: Make this less waste once we have more particles
-                    if draw_p_type == ParticleType::Water {
-                        draw_p_type = ParticleType::Sand;
-                    } else {
-                        draw_p_type = ParticleType::Water;
-                    }
+                Event::Quit {..} |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
                 },
-                Event::MouseButtonPressed { button: Button::LEFT, x, y } => {
-                    is_depressed = true;
-                    context.mouse_x = x;
-                    context.mouse_y = y;
-                },
-                Event::MouseButtonReleased { button: Button::LEFT, .. } => {
-                    is_depressed = false;
-                },
-                Event::MouseMoved { x, y } => {
-                    context.mouse_x = x;
-                    context.mouse_y = y;
-                }
-                _ => { /* Do nothing */ }
+                _ => {}
             }
         }
 
-        // Activate the window for OpenGL rendering
-        window.set_active(true);
+        //canvas.clear();
 
-        window.clear(Color::BLACK);
-
-        // FIXME: Run on a UI thread instead
-
-        if is_depressed {
-            insert_particle(physics.get_grid(), &context, &draw_p_type);
+        unsafe {
+            gl::ClearColor(0.6, 0.0, 0.8, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
         }
 
-        let curr_time = clock.elapsed_time().as_seconds();
+        let curr_time = SystemTime::now()
+            .duration_since(program_epoch)
+            .unwrap()
+            .as_secs_f32();
+
         let curr_tick = (curr_time / tick_time) as u32;
 
         if curr_tick > prev_tick {
@@ -264,39 +154,6 @@ fn main() {
             }
         }
 
-        renderer.render(&mut window, physics.get_grid());
-
-        // Render the FPS
-        fps_counter.tick(curr_time);
-        window.draw(fps_counter.get_display_text());
-
-        let mut debug_text = Text::default();
-
-        debug_text.set_font(&debug_font);
-        debug_text.set_position(Vector2f::new(0.0, 24.0));
-        debug_text.set_character_size(24);
-        debug_text.set_fill_color(Color::WHITE);
-
-        let x = context.get_mouse_grid_x();
-        let y = context.get_mouse_grid_y();
-        let grid = physics.get_grid();
-
-        if grid.in_bounds(x, y) {
-            let particle = grid.get(x, y).clone();
-            let fr = particle.fill_ratio;
-            let p_type = particle.p_type;
-            let bearing = particle.bearing;
-
-            let water_count = sum_water(grid);
-
-            debug_text.set_string(&format!("{}, {:?}, {:?}, {}", fr, bearing, p_type, water_count));
-        }
-
-        window.draw(&debug_text);
-
-        draw_overlay(&mut window, &context, &grid);
-
-        // End the current frame and display its contents on screen
-        window.display();
+        canvas.present();
     }
 }
--- src/render.rs
+++ src/render.rs
@@ -1,27 +1,31 @@
+extern crate gl;
+
 use sfml::graphics::{RenderStates, RenderTarget,RenderWindow, RectangleShape, Shader};
 use sfml::system::{Vector2f};
 
 use crate::grid::{ParticleGrid, ParticleType};
 use crate::{RenderContext};
 
+use std::ffi::CString;
+
 pub trait Renderer {
-    fn render(&mut self, window: &mut RenderWindow, grid: &ParticleGrid);
+    fn render(&mut self, grid: &ParticleGrid);
 }
 
-pub struct GlslRenderer<'a> {
+pub struct SfmlRenderer<'a> {
     shader: Shader<'a>,
     rect: RectangleShape<'a>,
     particles: Vec<f32>,
 }
 
-impl <'a> GlslRenderer<'a> {
-    pub fn new(shader_path: String, context: &RenderContext) -> GlslRenderer<'a> {
+impl <'a> SfmlRenderer<'a> {
+    pub fn new(shader_path: String, context: &RenderContext) -> SfmlRenderer<'a> {
         let mut shader = Shader::from_file(None, None, Some(shader_path.as_str())).unwrap();
         shader.set_uniform_int("win_height", context.win_height as i32);
         shader.set_uniform_float("scale", context.scale);
         shader.set_uniform_int("grid_width", context.grid_width as i32);
 
-        GlslRenderer {
+        SfmlRenderer {
             rect: RectangleShape::with_size(
                 Vector2f::new(context.win_width as f32, context.win_height as f32)
             ),
@@ -31,8 +35,8 @@ impl <'a> GlslRenderer<'a> {
     }
 }
 
-impl <'a> Renderer for GlslRenderer<'a> {
-    fn render(&mut self, window: &mut RenderWindow, grid: &ParticleGrid) {
+impl <'a> Renderer for SfmlRenderer<'a> {
+    fn render(&mut self, grid: &ParticleGrid) {
         for (i, p) in grid.grid.iter().enumerate() {
             let val = match p.p_type {
                 ParticleType::Water => 1.0,
@@ -46,6 +50,66 @@ impl <'a> Renderer for GlslRenderer<'a> {
 
         let mut states = RenderStates::default();
         states.set_shader(Some(&self.shader));
-        window.draw_with_renderstates(&self.rect, &states);
+        //window.draw_with_renderstates(&self.rect, &states);
     }
+}
+
+pub struct GlslRenderer {
+}
+
+use gl::types::{GLfloat, GLenum, GLuint, GLint, GLchar, GLsizeiptr};
+
+impl GlslRenderer {
+    pub fn new(shader_path: String, context: &RenderContext) -> GlslRenderer {
+        let shader_src = std::fs::read_to_string(shader_path).expect("shader not found!");
+
+        compile_shader(&shader_src, gl::FRAGMENT_SHADER);
+
+        GlslRenderer {
+        }
+    }
+}
+
+impl Renderer for GlslRenderer {
+    fn render(&mut self, grid: &ParticleGrid) {
+    }
+}
+
+fn compile_shader(src: &str, ty: GLenum) -> GLuint {
+    let shader;
+    unsafe {
+        shader = gl::CreateShader(ty);
+        // Attempt to compile the shader
+        let c_str = CString::new(src.as_bytes()).unwrap();
+        gl::ShaderSource(shader, 1, &c_str.as_ptr(), std::ptr::null());
+        gl::CompileShader(shader);
+
+        // Get the compile status
+        let mut status = gl::FALSE as GLint;
+        gl::GetShaderiv(shader, gl::COMPILE_STATUS, &mut status);
+
+        // Fail on error
+        if status != (gl::TRUE as GLint) {
+            let mut len = 0;
+            gl::GetShaderiv(shader, gl::INFO_LOG_LENGTH, &mut len);
+
+            let mut buf = vec![32u8; len as usize];
+
+            gl::GetShaderInfoLog(
+                shader,
+                len,
+                std::ptr::null_mut(),
+                buf.as_mut_ptr() as *mut GLchar,
+            );
+
+            let message = String::from_utf8(buf.as_slice().to_vec())
+                .ok()
+                .expect("ShaderInfoLog not valid utf8");
+
+            println!("Shader compile error:\n{}", message);
+
+            panic!("Rubbish shader, rubbish programmer");
+        }
+    }
+    shader
 }
